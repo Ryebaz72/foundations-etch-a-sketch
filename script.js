0be3b61f5dfdc57f037183fdd3954f3ab138d79c
Kenneth Rough <kennethrough@gmail.com>
2024-10-14 15:15:19 +0100

    Add function for variable size grid

--- script.js
+++ script.js
@@ -6,37 +6,48 @@ let btn = document.createElement("button");
 buttonDiv.appendChild(btn);
 btn.textContent = "Grid Size";
 container.appendChild(gridDiv);
-let numSquares = 16;
-let squareSide = (960 / numSquares);
-console.log(squareSide);
-for (let i = 0; i < numSquares; i++) {
-    const col = document.createElement("div");
-    col.classList.add("square");
-    col.style.width = `${squareSide}px`;
-    col.style.height = `${squareSide}px`;
-    col.id = `div${i}`;
-    gridDiv.appendChild(col);
-    col.textContent = col.id;
-    for (let j = 1; j < numSquares; j++) {
-        const row = document.createElement("div");
-        row.style.width = `${squareSide}px`;
-        row.style.height = `${squareSide}px`;
-        row.classList.add("square");
-        row.id = `div${i}${j}`;
-        gridDiv.appendChild(row);
-        row.textContent = row.id;
+
+function createGrid(squares) {
+    let numSquares = squares;
+    let squareSide = (960 / numSquares);
+    for (let i = 0; i < numSquares; i++) {
+        const col = document.createElement("div");
+        col.classList.add("square");
+        col.style.width = `${squareSide}px`;
+        col.style.height = `${squareSide}px`;
+        col.id = `div${i}`;
+        gridDiv.appendChild(col);
+        for (let j = 1; j < numSquares; j++) {
+            const row = document.createElement("div");
+            row.style.width = `${squareSide}px`;
+            row.style.height = `${squareSide}px`;
+            row.classList.add("square");
+            row.id = `div${i}${j}`;
+            gridDiv.appendChild(row);
+        }
     }
+    gridDiv.addEventListener("mouseover", (event) => {
+        let target = event.target;
+        let hoverSquareID = target.id;
+        let hoverSquare = document.getElementById(hoverSquareID);
+        hoverSquare.style.backgroundColor = "red";
+    });
 }
-gridDiv.addEventListener("mouseover", (event) => {
-    let target = event.target;
-    let hoverSquareID = target.id;
-    //    console.log(hoverSquareID);
-    let hoverSquare = document.getElementById(hoverSquareID);
-    hoverSquare.style.backgroundColor = "red";
-});
+
 btn.addEventListener("click", (e) => {
-    numSquares = parseInt(prompt(`How many squares would you like displayed?
-        Maximum 100.`));
-    console.log(numSquares);
+    let validNum = false;
+    let numSquares = parseInt(prompt(`How many squares would you like displayed?
+    Maximum 100.`));
+    while (!validNum) {
+        if (numSquares <= 100) {
+            validNum = true;
+        } else {
+            numSquares = parseInt(prompt("Maximum value is 100"));
+        }
+    }
+    gridDiv.innerHTML = "";
+    container.replaceChild(gridDiv, gridDiv);
+    createGrid(numSquares);
 });
-console.log(numSquares);
+
+createGrid(16);
